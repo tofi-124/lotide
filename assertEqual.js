1e11a37a8e1bca37733bf0cb681cc93519e532e2
Tofik Mohammed <tofikahm@yahoo.com>
2021-11-22 21:28:44 +0000

    this is our implementation of assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("💯💯💯 Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("💀💀💀 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
