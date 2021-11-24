f0c11facd7415df42995326346cb4d958050cd47
Tofik Mohammed <tofikahm@yahoo.com>
2021-11-24 05:29:47 +0000

    minor issues fixed

---
+++ password.js
@@ -0,0 +1,26 @@
+function password(str) {
+  str = str.split("");
+  for (let i = 0; i < str.length; i++) {
+    switch (str[i]) {
+      case "a":
+        str[i] = 4;
+        break;
+      case "e":
+        str[i] = 3;
+        break;
+      case "o":
+        str[i] = 0;
+        break;
+      case "l":
+        str[i] = 1;
+        break;
+    }
+  }
+  str = str.join("");
+  return str;
+}
+
+const args = process.argv.slice(2);
+args.forEach((element) => {
+  console.log(password(element));
+});
