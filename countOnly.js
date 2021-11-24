5b2cb9ca374f9e792a916df9734f2e5e603e0c49
Tofik Mohammed <tofikahm@yahoo.com>
2021-11-24 20:50:02 +0000

    implmenting countOnly.js

---
+++ countOnly.js
@@ -0,0 +1,46 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💯💯💯 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`💀💀💀 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countOnly = function(arrs, obj) {
+  const results = {};
+  for (const arr of arrs) {
+    if (obj[arr]) {
+      if (results[arr]) {
+        results[arr] += 1;
+      } else {
+        results[arr] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+///Test
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe",
+];
+
+const result1 = countOnly(firstNames, {
+  Jason: true,
+  Karima: true,
+  Fang: true,
+  Agouhanna: false,
+});
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
