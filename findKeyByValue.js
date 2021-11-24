695974cb4d72dfe8b8dfe104ddeeee1eb78dd83b
Tofik Mohammed <tofikahm@yahoo.com>
2021-11-24 23:32:24 +0000

    implmenting findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,32 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`💯💯💯 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`💀💀💀 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function (obj, str) {
+  let rstr = "";
+  for (let keys in obj) {
+    if (obj[`${keys}`] === str) {
+      rstr = keys;
+    }
+  }
+  if (rstr !== "") {
+    return rstr;
+  } else return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  acion: "Bruce_lee",
+  drama: "The Wire",
+  series: "Freinds",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Freinds"), "series");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Bruce_lee"), "acion");
