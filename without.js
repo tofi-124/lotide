daa5f10b85d2c6932ed422bc62acb35d97390d11
Tofik Mohammed <tofikahm@yahoo.com>
2021-11-24 01:10:17 +0000

    This here  filter our data by removing some unwanted items

---
+++ without.js
@@ -0,0 +1,34 @@
+const assertArraysEqual = function (arr1, arr2) {
+  let i;
+  let flag = true;
+
+  for (i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      flag = false;
+    }
+  }
+
+  if (flag === true) {
+    console.log(`💯💯💯 Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`💀💀💀 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const without = function (arr1, arr2) {
+  let newArr = [];
+  for (i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      newArr.push(arr1[i]);
+    }
+  }
+  return newArr;
+};
+
+console.log(without([1, 2, 3], [1]));
+console.log(without(["1", "2", "3"], [1, 2, "3"]));
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
