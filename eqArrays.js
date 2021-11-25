13f22142ac746dece058ebef23fdf9c814a49b07
Tofik Mohammed <tofikahm@yahoo.com>
2021-11-25 23:42:26 +0000

    minor debug

--- eqArrays.js
+++ eqArrays.js
@@ -9,14 +9,14 @@ const assertEqual = function(actual, expected) {
 const eqArrays = function(arr1, arr2) {
   let i;
   let flag = [];
-
+  
   if (arr1.length === arr2.length) {
     for (i = 0; i < arr1.length; i++) {
       if (arr1[i] === arr2[i]) {
         flag.push(true);
-      } else flag.push(false);
+      } else return false;
     }
-  } else flag.push(false);
+  } else return false;
 
   if (flag.includes(false)) {
     return false;
