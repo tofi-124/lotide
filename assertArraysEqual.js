aa9f6dfc4d2f7b4c230cd76778bc7e795a7c402f
Tofik Mohammed <tofikahm@yahoo.com>
2021-11-29 22:19:39 +0000

    Refactoring

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  let i;
-  let flag = [];
-
-  if (arr1.length === arr2.length) {
-    for (i = 0; i < arr1.length; i++) {
-      if (arr1[i] === arr2[i]) {
-        flag.push(true);
-      } else flag.push(false);
-    }
-  } else flag.push(false);
-
-  if (flag.includes(false)) {
-    return false;
-  } else return true;
-};
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function(arr1, arr2) {
   let chk = eqArrays(arr1, arr2);
@@ -25,4 +10,5 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
+module.exports = assertArraysEqual;
+
