a6fbda863c567639ff0e604bc6646d5c3ff46427
Tofik Mohammed <tofikahm@yahoo.com>
2021-11-29 22:12:30 +0000

    Refactoring

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const eqArrays = function(arr1, arr2) {
   let i;
@@ -23,8 +16,5 @@ const eqArrays = function(arr1, arr2) {
   } else return true;
 };
 
+module.exports = eqArrays;
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
