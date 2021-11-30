33d3d5bbc3a810d8138058263ec8d7f49839f817
Tofik Mohammed <tofikahm@yahoo.com>
2021-11-30 08:50:01 +0000

    adding index.js for out lotide" "

--- index.js
+++ index.js
@@ -2,9 +2,19 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findkey = require('./findkey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const mapII = require('./mapII');
+const middle = require('./middle');
 
-module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle
-};
+let indexObj = {middle, mapII, map, letterPositions, head,tail,middle, assertArraysEqual, assertEqual, countLetters, countOnly, eqArrays, eqObjects, findkey, findKeyByValue, flatten}
+module.exports =indexObj
